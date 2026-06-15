6125bba436af1c1f2344e1e021de712d6b2d4b18
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:34:11 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/combat/Velocity.java
+++ src/main/java/com/argon/client/module/impl/combat/Velocity.java
@@ -1,20 +1,9 @@
 package com.argon.client.module.impl.combat;
 
 import com.argon.client.module.Module;
-import net.minecraft.entity.damage.DamageSource;
 
 public class Velocity extends Module {
-
     public Velocity() {
-        super("Velocity", "Reduces knockback");
-    }
-
-    @Override
-    public void onTick() {
-        // Simple placeholder – real implementation would listen to damage events via mixins
-        // Here we just set a very low knockback strength each tick
-        if (mc.player != null) {
-            mc.player.setVelocity(mc.player.getVelocity().multiply(0.9));
-        }
+        super("Velocity", "Stub module");
     }
 }
