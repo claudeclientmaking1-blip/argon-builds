84cd310a2bc306bd1dbf1a7324293997ea016f2c
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:13 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/combat/Reach.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.combat;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class Reach extends Module {
+    public Reach() {
+        super("Reach", Category.COMBAT);
+    }
+}
