ee11edc418b9156e8b128c9745542a3bba5269f0
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:28:31 +0530

    Build Argon Client

--- src/main/java/com/argon/client/gui/HudRenderer.java
+++ src/main/java/com/argon/client/gui/HudRenderer.java
@@ -1,22 +1,25 @@
 package com.argon.client.gui;
 
-import com.argon.client.ArgonClient;
 import com.argon.client.module.Module;
-import net.minecraft.client.MinecraftClient;
+import com.argon.client.module.ModuleManager;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class HudRenderer {
-    public static void render(DrawContext context, float tickDelta) {
-        MinecraftClient mc = MinecraftClient.getInstance();
-        TextRenderer textRenderer = mc.textRenderer;
-        MatrixStack matrices = context.getMatrices();
+
+    private final TextRenderer font;
+
+    public HudRenderer(TextRenderer font) {
+        this.font = font;
+    }
+
+    public void render(DrawContext context) {
         int y = 10;
-        for (Module module : ArgonClient.MODULE_MANAGER.getModules()) {
+        for (Module module : ModuleManager.getModules()) {
             if (module.isEnabled()) {
-                String text = module.getName() + " [ON]";
-                textRenderer.draw(matrices, text, 10, y, 0xFFFFFF);
+                String text = module.getName();
+                context.drawText(font, text, 10, y, 0xFFFFFF, true);
                 y += 10;
             }
         }
