d96d71a2efc2e1543e18736fa80855056c8b4e7d
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:28:24 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/combat/Criticals.java
+++ src/main/java/com/argon/client/module/impl/combat/Criticals.java
@@ -1,10 +1,21 @@
 package com.argon.client.module.impl.combat;
 
 import com.argon.client.module.Module;
-import com.argon.client.module.Module.Category;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.Vec3d;
 
 public class Criticals extends Module {
+
     public Criticals() {
-        super("Critical", Category.COMBAT);
+        super("Criticals", "Force critical hits on attack");
+    }
+
+    @Override
+    public void onTick() {
+        // Simple placeholder: make the player slightly jump each attack cooldown
+        if (mc.player == null) return;
+        if (mc.interactionManager.getAttackCooldown() == 0 && mc.player.isOnGround()) {
+            mc.player.jump();
+        }
     }
 }
