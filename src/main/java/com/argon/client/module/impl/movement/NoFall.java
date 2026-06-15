c37e2ab1632c0fdd899e5dc764c5c319a963e0fb
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:28:26 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/movement/NoFall.java
+++ src/main/java/com/argon/client/module/impl/movement/NoFall.java
@@ -1,10 +1,18 @@
 package com.argon.client.module.impl.movement;
 
 import com.argon.client.module.Module;
-import com.argon.client.module.Module.Category;
 
 public class NoFall extends Module {
+
     public NoFall() {
-        super("NoFall", Category.MOVEMENT);
+        super("NoFall", "Prevents fall damage");
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+        if (mc.player.fallDistance > 2.0f) {
+            mc.player.fallDistance = 0.0f;
+        }
     }
 }
