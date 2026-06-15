572669088c8865808abfc798e62df4eb171e9145
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:34:14 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/movement/Speed.java
+++ src/main/java/com/argon/client/module/impl/movement/Speed.java
@@ -1,22 +1,9 @@
 package com.argon.client.module.impl.movement;
 
 import com.argon.client.module.Module;
-import net.minecraft.util.math.Vec3d;
 
 public class Speed extends Module {
-
-    private final double multiplier = 1.5;
-
     public Speed() {
-        super("Speed", "Increases player movement speed");
-    }
-
-    @Override
-    public void onTick() {
-        if (mc.player == null) return;
-        if (mc.player.isSprinting()) {
-            Vec3d vel = mc.player.getVelocity();
-            mc.player.setVelocity(vel.x * multiplier, vel.y, vel.z * multiplier);
-        }
+        super("Speed", "Stub module");
     }
 }
