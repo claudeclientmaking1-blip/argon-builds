15ee32b7b354206bf80c63dcff524ce50e7a7bc8
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:20 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/player/AutoClicker.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.player;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class AutoClicker extends Module {
+    public AutoClicker() {
+        super("AutoClicker", Category.PLAYER);
+    }
+}
