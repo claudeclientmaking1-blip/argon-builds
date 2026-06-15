6912af13323cdfab68c9e92ddfd972dc4760f8df
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:17 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/movement/NoFall.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.movement;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class NoFall extends Module {
+    public NoFall() {
+        super("NoFall", Category.MOVEMENT);
+    }
+}
