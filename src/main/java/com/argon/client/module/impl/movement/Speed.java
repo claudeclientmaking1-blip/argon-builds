aba204e8cbf7a2269636be55bc8b78c91e97d9a6
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:46:53 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/movement/Speed.java
+++ src/main/java/com/argon/client/module/impl/movement/Speed.java
@@ -1,9 +1,30 @@
 package com.argon.client.module.impl.movement;
 
 import com.argon.client.module.Module;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class Speed extends Module {
+
+    private static final double MULTIPLIER = 1.5;
+
     public Speed() {
-        super("Speed", "Stub module");
+        super("Speed");
+    }
+
+    @Override
+    protected void onEnable() {}
+
+    @Override
+    protected void onDisable() {}
+
+    @Override
+    public void onTick(MinecraftClient client) {
+        PlayerEntity player = client.player;
+        if (player == null) return;
+
+        double base = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).getBaseValue();
+        player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue(base * MULTIPLIER);
     }
 }
