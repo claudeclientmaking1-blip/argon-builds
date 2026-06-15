1a78075e1fe4494b589978b22c39878dde4016d5
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:34:18 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/player/AutoClicker.java
+++ src/main/java/com/argon/client/module/impl/player/AutoClicker.java
@@ -1,29 +1,9 @@
 package com.argon.client.module.impl.player;
 
 import com.argon.client.module.Module;
-import net.minecraft.util.Hand;
 
 public class AutoClicker extends Module {
-
-    private int tickCounter = 0;
-    private final int clicksPerSecond = 12;
-
     public AutoClicker() {
-        super("AutoClicker", "Automatically clicks when holding mouse");
-    }
-
-    @Override
-    public void onTick() {
-        if (mc.player == null) return;
-        if (mc.options.useKey.isPressed()) {
-            tickCounter++;
-            if (tickCounter >= 20 / clicksPerSecond) {
-                mc.interactionManager.attackEntity(mc.player, mc.targetedEntity);
-                mc.player.swingHand(Hand.MAIN_HAND);
-                tickCounter = 0;
-            }
-        } else {
-            tickCounter = 0;
-        }
+        super("AutoClicker", "Stub module");
     }
 }
