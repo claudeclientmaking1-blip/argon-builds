644890e01626e116aaa55c8557d2686c584bec6e
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:18 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/movement/Sprint.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.movement;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class Sprint extends Module {
+    public Sprint() {
+        super("Sprint", Category.MOVEMENT);
+    }
+}
