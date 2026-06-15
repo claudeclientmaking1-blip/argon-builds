97228d9557d1c0caca6e460252c285584a743778
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:11 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/combat/KillAura.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.combat;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class KillAura extends Module {
+    public KillAura() {
+        super("KillAura", Category.COMBAT);
+    }
+}
