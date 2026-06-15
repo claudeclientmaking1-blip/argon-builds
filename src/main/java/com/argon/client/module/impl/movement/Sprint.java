92d482e6f6ad9f2878a0492f06ba4ca30c2db149
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:46:56 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/movement/Sprint.java
+++ src/main/java/com/argon/client/module/impl/movement/Sprint.java
@@ -1,9 +1,28 @@
 package com.argon.client.module.impl.movement;
 
 import com.argon.client.module.Module;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class Sprint extends Module {
+
     public Sprint() {
-        super("Sprint", "Stub module");
+        super("Sprint");
+    }
+
+    @Override
+    protected void onEnable() {}
+
+    @Override
+    protected void onDisable() {}
+
+    @Override
+    public void onTick(MinecraftClient client) {
+        PlayerEntity player = client.player;
+        if (player == null) return;
+
+        if (!player.isSprinting() && player.input.movementForward > 0) {
+            player.setSprinting(true);
+        }
     }
 }
