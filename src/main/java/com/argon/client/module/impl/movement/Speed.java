c484b53aac694ce548f31041eb1f7ec4f984523d
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:16 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/movement/Speed.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.movement;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class Speed extends Module {
+    public Speed() {
+        super("Speed", Category.MOVEMENT);
+    }
+}
