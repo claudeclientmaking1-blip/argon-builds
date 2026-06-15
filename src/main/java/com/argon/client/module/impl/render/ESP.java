d1d9dedfc2daefde4480d5cdf3acbd87ff2ea103
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:21 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/render/ESP.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.render;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class ESP extends Module {
+    public ESP() {
+        super("ESP", Category.RENDER);
+    }
+}
