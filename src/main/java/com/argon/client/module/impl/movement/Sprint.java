904629886c239223c1eac3152783ea2fc3e78d26
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:28:27 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/movement/Sprint.java
+++ src/main/java/com/argon/client/module/impl/movement/Sprint.java
@@ -1,10 +1,18 @@
 package com.argon.client.module.impl.movement;
 
 import com.argon.client.module.Module;
-import com.argon.client.module.Module.Category;
 
 public class Sprint extends Module {
+
     public Sprint() {
-        super("Sprint", Category.MOVEMENT);
+        super("Sprint", "Automatically starts sprinting");
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+        if (!mc.player.isSprinting() && mc.player.input.movementForward > 0.8f) {
+            mc.player.setSprinting(true);
+        }
     }
 }
