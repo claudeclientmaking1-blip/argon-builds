ec9f20e9c8e2c38f408a5afef8b8212d49d3823d
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:28:32 +0530

    Build Argon Client

--- src/main/java/com/argon/client/gui/ClickGui.java
+++ src/main/java/com/argon/client/gui/ClickGui.java
@@ -1,42 +1,37 @@
 package com.argon.client.gui;
 
-import com.argon.client.ArgonClient;
 import com.argon.client.module.Module;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
+import com.argon.client.module.ModuleManager;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
 public class ClickGui extends Screen {
-    public ClickGui() {
-        super(Text.of("Argon ClickGui"));
+
+    protected ClickGui() {
+        super(Text.literal("Argon Click GUI"));
     }
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.drawBackground(context.getMatrices(), 0x55000000, mouseX, mouseY);
-        super.render(context, mouseX, mouseY, delta);
-        MatrixStack matrices = context.getMatrices();
+    protected void init() {
         int y = 20;
-        for (Module module : ArgonClient.MODULE_MANAGER.getModules()) {
-            String status = module.isEnabled() ? "[ON]" : "[OFF]";
-            String line = module.getName() + " " + status;
-            context.drawText(mc.textRenderer, line, 20, y, 0xFFFFFF, false);
-            y += 15;
+        for (Module module : ModuleManager.getModules()) {
+            ButtonWidget btn = ButtonWidget.builder(
+                    Text.literal(module.getName() + (module.isEnabled() ? " [ON]" : " [OFF]")),
+                    button -> {
+                        module.toggle();
+                        button.setMessage(Text.literal(module.getName() + (module.isEnabled() ? " [ON]" : " [OFF]")));
+                    })
+                    .position(20, y)
+                    .size(120, 20)
+                    .build();
+            addDrawableChild(btn);
+            y += 25;
         }
     }
 
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        int y = 20;
-        for (Module module : ArgonClient.MODULE_MANAGER.getModules()) {
-            if (mouseY >= y && mouseY <= y + 12 && mouseX >= 20 && mouseX <= 20 + 100) {
-                module.toggle();
-                return true;
-            }
-            y += 15;
-        }
-        return super.mouseClicked(mouseX, mouseY, button);
+    public boolean shouldPause() {
+        return false;
     }
 }
