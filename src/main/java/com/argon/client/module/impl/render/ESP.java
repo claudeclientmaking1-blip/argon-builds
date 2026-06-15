0243a7d2b1dd1334b6db106ee97292156f274fd5
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:28:30 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/render/ESP.java
+++ src/main/java/com/argon/client/module/impl/render/ESP.java
@@ -1,10 +1,20 @@
 package com.argon.client.module.impl.render;
 
 import com.argon.client.module.Module;
-import com.argon.client.module.Module.Category;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 
 public class ESP extends Module {
+
     public ESP() {
-        super("ESP", Category.RENDER);
+        super("ESP", "Renders boxes around players");
+    }
+
+    @Override
+    public void onTick() {
+        // Rendering is done via a mixin on WorldRenderEvents – placeholder here.
     }
 }
