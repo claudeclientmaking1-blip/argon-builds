9a1df7d529c7b1c90c09e124fad425f653792550
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:14:15 +0530

    Build Argon Client

---
+++ src/main/java/com/argon/client/module/impl/combat/Criticals.java
@@ -0,0 +1,10 @@
+package com.argon.client.module.impl.combat;
+
+import com.argon.client.module.Module;
+import com.argon.client.module.Module.Category;
+
+public class Criticals extends Module {
+    public Criticals() {
+        super("Critical", Category.COMBAT);
+    }
+}
