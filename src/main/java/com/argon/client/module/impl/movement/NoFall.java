d2efa12f1f98ea0223e97f333aa1d7d06c27841f
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-15 18:46:54 +0530

    Build Argon Client

--- src/main/java/com/argon/client/module/impl/movement/NoFall.java
+++ src/main/java/com/argon/client/module/impl/movement/NoFall.java
@@ -1,9 +1,32 @@
 package com.argon.client.module.impl.movement;
 
 import com.argon.client.module.Module;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Vec3d;
 
 public class NoFall extends Module {
+
     public NoFall() {
-        super("NoFall", "Stub module");
+        super("NoFall");
+    }
+
+    @Override
+    protected void onEnable() {}
+
+    @Override
+    protected void onDisable() {}
+
+    @Override
+    public void onTick(MinecraftClient client) {
+        PlayerEntity player = client.player;
+        if (player == null) return;
+
+        if (player.fallDistance > 2.0f) {
+            // Reset fall distance to prevent damage
+            player.fallDistance = 0.0f;
+            // Slight upward motion to keep in air
+            player.setVelocity(new Vec3d(player.getVelocity().x, 0.05, player.getVelocity().z));
+        }
     }
 }
