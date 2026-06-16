8a3712205d5bf0996d69fd8b735b622fa305c478
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-16 10:47:16 +0530

    chore: push verified base files

--- src/main/java/com/argon/client/module/impl/movement/Speed.java
+++ src/main/java/com/argon/client/module/impl/movement/Speed.java
@@ -1,30 +1,16 @@
 package com.argon.client.module.impl.movement;
-
 import com.argon.client.module.Module;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.player.PlayerEntity;
-
 public class Speed extends Module {
-
-    private static final double MULTIPLIER = 1.5;
-
-    public Speed() {
-        super("Speed");
+    public Speed() { super("Speed", Category.MOVEMENT); }
+    @Override public void onEnable() {
+        if (mc.player == null) return;
+        var i = mc.player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (i != null) i.setBaseValue(0.2);
     }
-
-    @Override
-    protected void onEnable() {}
-
-    @Override
-    protected void onDisable() {}
-
-    @Override
-    public void onTick(MinecraftClient client) {
-        PlayerEntity player = client.player;
-        if (player == null) return;
-
-        double base = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).getBaseValue();
-        player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue(base * MULTIPLIER);
+    @Override public void onDisable() {
+        if (mc.player == null) return;
+        var i = mc.player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (i != null) i.setBaseValue(0.1);
     }
 }
