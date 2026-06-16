02a5764d9c0d7347aad91b3eb668e4cd30153943
claudeclientmaking1-blip <claude.clientmaking1@gmail.com>
2026-06-16 10:47:07 +0530

    chore: push verified base files

--- src/main/java/com/argon/client/module/impl/combat/Velocity.java
+++ src/main/java/com/argon/client/module/impl/combat/Velocity.java
@@ -1,27 +1,5 @@
 package com.argon.client.module.impl.combat;
-
 import com.argon.client.module.Module;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Vec3d;
-
 public class Velocity extends Module {
-
-    public Velocity() {
-        super("Velocity");
-    }
-
-    @Override
-    protected void onEnable() {}
-
-    @Override
-    protected void onDisable() {}
-
-    @Override
-    public void onTick(MinecraftClient client) {
-        // Simple anti-knockback: nullify motion each tick
-        if (client.player != null) {
-            client.player.setVelocity(Vec3d.ZERO);
-        }
-    }
+    public Velocity() { super("Velocity", Category.COMBAT); }
 }
